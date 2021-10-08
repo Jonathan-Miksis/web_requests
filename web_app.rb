39b7d76464bfe11002f549eff17cea0c129df81f
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-08 12:45:00 -0400

    added option for user to choose farenheit or celcius

--- web_app.rb
+++ web_app.rb
@@ -6,11 +6,23 @@ while true
   if city == "quit"
     break
   end
-  response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
-  min_temp = response.parse(:json)["main"]["temp_min"]
-  max_temp = response.parse(:json)["main"]["temp_max"]
-  conditions = response.parse(:json)["weather"][0]["description"]
-  wind_speed = response.parse(:json)["wind"]["speed"]
-
-  p "The weather in #{city} has a low of #{min_temp} and high of #{max_temp}. The conditions are #{conditions} with a a wind speed of #{wind_speed}."
+  p "Would you like your temperature in Celcius or Farenheit?"
+  temperature_unit = gets.chomp
+  if temperature_unit == "Celcius"
+    response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=metric")
+    min_temp = response.parse(:json)["main"]["temp_min"]
+    max_temp = response.parse(:json)["main"]["temp_max"]
+    conditions = response.parse(:json)["weather"][0]["description"]
+    wind_speed = response.parse(:json)["wind"]["speed"]
+  
+    p "The weather in #{city} has a low of #{min_temp} and high of #{max_temp}. The conditions are #{conditions} with a a wind speed of #{wind_speed}."
+  elsif temperature_unit == "Farenheit"
+    response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}&units=imperial")
+    min_temp = response.parse(:json)["main"]["temp_min"]
+    max_temp = response.parse(:json)["main"]["temp_max"]
+    conditions = response.parse(:json)["weather"][0]["description"]
+    wind_speed = response.parse(:json)["wind"]["speed"]
+  
+    p "The weather in #{city} has a low of #{min_temp} and high of #{max_temp}. The conditions are #{conditions} with a a wind speed of #{wind_speed}."
+  end
 end
