a098b54931917bbfb6bb9610cbd7dc0e055e8f59
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-07 21:26:42 -0400

    made API key in environment variable

--- requests.rb
+++ requests.rb
@@ -1,5 +1,11 @@
+# require 'http'
+
+# response = HTTP.get("https://data.ny.gov/resource/xjya-f8ng.json")
+
+# p response.parse(:json)
+
 require 'http'
 
-response = HTTP.get("https://data.ny.gov/resource/xjya-f8ng.json")
+response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=boston&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
 
 p response.parse(:json)
