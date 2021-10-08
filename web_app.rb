706bfb3d3f0dc821b9933dcd3f490b69cd27e3af
Jonathan-Miksis <jmiksis13@gmail.com>
2021-10-08 12:26:36 -0400

    removed method and added statement to print requested information from user

--- web_app.rb
+++ web_app.rb
@@ -1,20 +1,12 @@
 require 'http'
 
-def forecast(input_city)
-  city = input_city
-  response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
-  p response.parse(:json)["main"]["temp_min"]
-  p response.parse(:json)["main"]["temp_max"]
-  p response.parse(:json)["weather"][0]["description"]
-  p response.parse(:json)["wind"]["speed"]
-end
-
-p "Please give me a city you'd like to see the weather in"
+p "Please enter a city and I'll give you the current weather"
 city = gets.chomp
 
-p "If you would like to see the forecast, please put F. If you would like to "
-input = gets.chomp
+response = HTTP.get("https://api.openweathermap.org/data/2.5/weather?q=#{city}&appid=#{ENV["OPEN_WEATHER_API_KEY"]}")
+min_temp = response.parse(:json)["main"]["temp_min"]
+max_temp = response.parse(:json)["main"]["temp_max"]
+conditions = response.parse(:json)["weather"][0]["description"]
+wind_speed = response.parse(:json)["wind"]["speed"]
 
-if gets.chomp == "F"
-  forecast(city)
-end
+p "The weather in #{city} has a low of #{min_temp} and high of #{max_temp}. The conditions are #{conditions} with a a wind speed of #{wind_speed}."
